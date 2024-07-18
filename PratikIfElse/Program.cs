6771f4703e06c76e2716b80afa9fe3f35190fe64
sefayilmaz <sefaylmaz14@gmail.com>
2024-07-18 18:23:26 +0300

    Add project files.

---
+++ PratikIfElse/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.WriteLine("Lütfen Bir Sayı Giriniz:");
+
+string giris = Console.ReadLine();
+int sayi = Convert.ToInt32(giris);
+
+if (sayi < 10)
+{
+    Console.WriteLine("Sayı 10'dan küçüktür.");
+    if (sayi % 2 == 0)
+    {
+        Console.WriteLine("Sayı Çift sayıdır.");
+    }
+    else
+    {
+        Console.WriteLine("Sayı Tek Sayıdır.");
+    }
+    
+}
+else if (sayi == 10)
+{
+    Console.WriteLine("Sayı 10'a eşittir");
+    Console.WriteLine("Sayı Çift Sayıdır.");
+}
+
+else
+{
+    Console.WriteLine("Sayı 10'dan büyüktür.");
+    if (sayi % 2 == 0)
+    {
+        Console.WriteLine("Sayı Çift Sayıdır.");
+    }
+    else
+    {
+        Console.WriteLine("Sayı Tek Sayıdır.");
+    }
+}
